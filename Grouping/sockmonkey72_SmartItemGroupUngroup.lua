62d889226345d5b25d4e4d00c34d7d1552533667
Jeremy Bernstein <jeremy.d.bernstein@googlemail.com>
2023-01-11 21:08:14 +0100

    sockmonkey72_SmartItemGroupUngroup.lua: don't create a new group (index) when updating an existing group. Thanks X-Raym

--- Grouping/sockmonkey72_SmartItemGroupUngroup.lua
+++ Grouping/sockmonkey72_SmartItemGroupUngroup.lua
@@ -1,7 +1,8 @@
 -- @description Smart item group/ungroup
 -- @author sockmonkey72
--- @version 1.0
--- @changelog 1.0 initial upload
+-- @version 1.1
+-- @changelog
+--   * When updating a group, don't change the group index (thanks X-Raym for noticing)
 -- @about
 --   # Smart item group/ungroup
 --   Thanks to daxliniere for the following specification:
@@ -10,64 +11,85 @@
 --   * If I select 3 items, some of which are part of a group and some ungrouped, the ungrouped items should be placed into the existing group.
 --   * If I select 3 items, some of which are part of one group and some another group, the grouping should be removed.
 
-function allSelectedItemsAreInTheSameGroup()
-  local count = reaper.CountSelectedMediaItems(0)
+local r = reaper
+
+local function allSelectedItemsAreInTheSameGroup()
+  local count = r.CountSelectedMediaItems(0)
   local grpid
   local sawzero = false
   local samegroup = true
 
   for i = 0, count - 1 do
-    local item = reaper.GetSelectedMediaItem(0, i)
+    local item = r.GetSelectedMediaItem(0, i)
     if item then
-      local itemid = reaper.GetMediaItemInfo_Value(item, "I_GROUPID")
-      if grpid == nil then
-        grpid = itemid
-      elseif itemid ~= grpid then
-        samegroup = false
+      local itemid = r.GetMediaItemInfo_Value(item, "I_GROUPID")
+      if itemid ~= 0 then
+        if grpid == nil then
+          grpid = itemid
+        elseif itemid ~= grpid then
+          samegroup = false
+        end
+      else
+        sawzero = true
       end
-      if itemid == 0 then sawzero = true end
     end
   end
 
   if sawzero == false then
-    return 1 -- everything is in a group: 1 (ungroup)
-  elseif samegroup == false then
-    return 0 -- something is unselected: 0 (update group)
+    return 1, nil -- everything is in a group: 1 (ungroup)
+  elseif samegroup == true then
+    return 0, grpid -- something is unselected, but everything else is in the same group
   else
-    return -1 -- nothing is grouped: -1 (create new group)
+    return -1, nil -- some mix of unselected and different groups: -1 (create new group)
   end
 end
 
-function removeItemsFromAllGroups()
-  reaper.Main_OnCommandEx(40033, 0) -- Ungroup native command
+local function removeItemsFromAllGroups()
+  r.ShowConsoleMsg('un group\n')
+  r.Main_OnCommandEx(40033, 0) -- Ungroup native command
 end
 
-function createGroupFromSelection()
-  reaper.Main_OnCommandEx(40033, 0) -- Ungroup native command
-  reaper.Main_OnCommandEx(40032, 0) -- Group native command
+local function updateGroupFromSelection(grpid)
+  r.ShowConsoleMsg('update group\n')
+  local count = r.CountSelectedMediaItems(0)
+  for i = 0, count - 1 do
+    local item = r.GetSelectedMediaItem(0, i)
+    if item then
+      r.SetMediaItemInfo_Value(item, "I_GROUPID", grpid)
+    end
+  end
+end
+
+local function createGroupFromSelection()
+  r.ShowConsoleMsg('create group\n')
+  r.Main_OnCommandEx(40033, 0) -- Ungroup native command
+  r.Main_OnCommandEx(40032, 0) -- Group native command
 end
 
 -----------------------------------------------------------
 -----------------------------------------------------------
 
-if reaper.CountSelectedMediaItems(0) > 0 then
+if r.CountSelectedMediaItems(0) > 0 then
 
-  reaper.PreventUIRefresh(1)
+  r.PreventUIRefresh(1)
 
   local undoDescription
-  reaper.Undo_BeginBlock2(0)
+  r.Undo_BeginBlock2(0)
 
-  status = allSelectedItemsAreInTheSameGroup()
+  local status, grpid = allSelectedItemsAreInTheSameGroup()
   if status == 1 then
     removeItemsFromAllGroups()
     undoDescription = "Remove Items From Group"
+  elseif status == 0 and grpid then
+    updateGroupFromSelection(grpid)
+    undoDescription = "Update Item Group"
   else
     createGroupFromSelection()
-    undoDescription = status == 0 and "Update Item Group" or "Create Item Group"
+    undoDescription = "Create Item Group"
   end
 
-  reaper.Undo_EndBlock2(0, undoDescription, -1)
-  reaper.UpdateArrange()
-  reaper.PreventUIRefresh(-1)
+  r.Undo_EndBlock2(0, undoDescription, -1)
+  r.UpdateArrange()
+  r.PreventUIRefresh(-1)
 
 end

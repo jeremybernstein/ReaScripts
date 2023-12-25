d3c56d50e75c2c3a0c632371309d2940f649f3b5
Jeremy Bernstein <jeremy.d.bernstein@googlemail.com>
2023-12-26 00:22:48 +0100

    Create Crossfade: fix edge cases (v1.3)

--- Items Editing/sockmonkey72_CreateCrossfadeUnderMouseCursor.lua
+++ Items Editing/sockmonkey72_CreateCrossfadeUnderMouseCursor.lua
@@ -1,13 +1,13 @@
 -- @description sockmonkey72_Create crossfade under mouse cursor
 -- @author sockmonkey72
--- @version 1.2
+-- @version 1.3
 -- @about
 --   # Creates a crossfade under the mouse cursor (if possible)
 -- @provides
 --   [main] sockmonkey72_CreateCrossfadeUnderMouseCursor.lua
 --   [main] sockmonkey72_CreateCrossfadeUnderMouseCursor_Config.lua
 -- @changelog
---   added justification preference (-1 (default) = left; 1 = right, 0 = centered)
+--   fixed up some edge cases and consolidate code a bit
 
 -- thanks to amagalma for some great examples of how it's done
 
@@ -38,7 +38,7 @@ end
 
 local xfadeshape = -1
 if xfadeshape < 0 or xfadeshape > 7 then
-  xfadeshape = tonumber(({reaper.get_config_var_string( "defxfadeshape" )})[2]) or 7
+  xfadeshape = tonumber(({reaper.get_config_var_string( 'defxfadeshape' )})[2]) or 7
 end
 
 local xfadetime
@@ -77,19 +77,19 @@ elseif use_time then
 end
 
 if not xfadetime then
-  xfadetime = (tonumber(({r.get_config_var_string( "defsplitxfadelen" )})[2]) or 0.01) * 2
+  xfadetime = (tonumber(({r.get_config_var_string( 'defsplitxfadelen' )})[2]) or 0.01) * 2
 end
 
 local fadelen = xfadetime -- override here if you want
 
 local retval, segment, details = r.BR_GetMouseCursorContext()
-if retval == "arrange" and segment == "track" and details == "item" then
+if retval == 'arrange' and segment == 'track' and details == 'item' then
   local item = r.BR_GetMouseCursorContext_Item()
   local pos = r.BR_GetMouseCursorContext_Position()
   local track = r.BR_GetMouseCursorContext_Track()
   local item_cnt = r.CountTrackMediaItems( track )
-  local itemstart = r.GetMediaItemInfo_Value(item, "D_POSITION")
-  local itemend = itemstart + r.GetMediaItemInfo_Value(item, "D_LENGTH")
+  local itemstart = r.GetMediaItemInfo_Value(item, 'D_POSITION')
+  local itemend = itemstart + r.GetMediaItemInfo_Value(item, 'D_LENGTH')
   local fixedlane = r.GetMediaTrackInfo_Value(track, 'I_FREEMODE') == 2
   local itemlane
   local item2, what
@@ -111,16 +111,16 @@ if retval == "arrange" and segment == "track" and details == "item" then
         if fixedlane then
           prevlane = r.GetMediaItemInfo_Value(previtem, 'I_FIXEDLANE')
         end
-        prevstart = r.GetMediaItemInfo_Value(previtem, "D_POSITION")
-        prevend = prevstart + r.GetMediaItemInfo_Value(previtem, "D_LENGTH")
+        prevstart = r.GetMediaItemInfo_Value(previtem, 'D_POSITION')
+        prevend = prevstart + r.GetMediaItemInfo_Value(previtem, 'D_LENGTH')
       end
       if (i < item_cnt - 1) then
         nextitem = r.GetTrackMediaItem(track, i + 1)
         if fixedlane then
           nextlane = r.GetMediaItemInfo_Value(nextitem, 'I_FIXEDLANE')
         end
-        nextstart = r.GetMediaItemInfo_Value(nextitem, "D_POSITION")
-        nextend = nextstart + r.GetMediaItemInfo_Value(nextitem, "D_LENGTH")
+        nextstart = r.GetMediaItemInfo_Value(nextitem, 'D_POSITION')
+        nextend = nextstart + r.GetMediaItemInfo_Value(nextitem, 'D_LENGTH')
       end
       break
     end
@@ -133,14 +133,14 @@ if retval == "arrange" and segment == "track" and details == "item" then
     item2 = previtem
     item2start = prevstart
     item2end = prevend
-    what = "itemStart"
+    what = 'itemStart'
     post('itemstart')
   end
   if not item2 and nextitemvalid and pos >= nextstart and pos <= nextend then
     item2 = nextitem
     item2start = nextstart
     item2end = nextend
-    what = "itemEnd"
+    what = 'itemEnd'
     post('itemend')
   end
   -- can this be consolidated into above, or could we miss extreme overlap situations?
@@ -150,59 +150,70 @@ if retval == "arrange" and segment == "track" and details == "item" then
       item2 = previtem
       item2start = prevstart
       item2end = prevend
-      what = "itemStart"
+      what = 'itemStart'
       post('itemstart (first half)')
     elseif nextitemvalid and pos > halftime then
       item2 = nextitem
       item2start = nextstart
       item2end = nextend
-      what = "itemEnd"
+      what = 'itemEnd'
       post('itemend (last half)')
     end
   end
 
   if item2 then
     -- verify that the items are close enough to crossfade (not sure if this check is necessary or correct, though)
-    if (what == "itemStart" and itemstart - item2end <= fadelen)
-      or (what == "itemEnd" and item2start - itemend <= fadelen)
+    if (what == 'itemStart' and itemstart - item2end <= fadelen)
+      or (what == 'itemEnd' and item2start - itemend <= fadelen)
     then
       ok = true
     end
   end
 
-  if ok then -- clear crossfade
-    r.Undo_BeginBlock2(0)
-
+  if ok then -- create crossfade
     local justlen = justification == -1 and fadelen or justification == 0 and fadelen * 0.5 or 0
 
-    if what == "itemStart" then
-      local newstart = itemstart
-      if itemstart > item2end - justlen then
-        newstart = item2end - justlen
-        r.BR_SetItemEdges(item, newstart, itemend)
-      elseif itemstart < item2end then
-        fadelen = item2end - itemstart
-      end
-      r.SetMediaItemInfo_Value(item, "D_FADEINLEN_AUTO", fadelen)
-      if item2end < newstart + fadelen then
-        r.BR_SetItemEdges(item2, item2start, newstart + fadelen)
-      end
-      r.SetMediaItemInfo_Value(item2, "D_FADEOUTLEN_AUTO", fadelen)
-    elseif what == "itemEnd" then
-      local newstart = item2start
-      if item2start > itemend - justlen then
-        newstart = itemend - justlen
-        r.BR_SetItemEdges(item2, newstart, item2end)
-      elseif item2start < itemend then
-        fadelen = itemend - item2start
-      end
-      r.SetMediaItemInfo_Value(item2, "D_FADEINLEN_AUTO", fadelen)
-      post(itemend, newstart, fadelen)
-      if itemend < newstart + fadelen then
-        r.BR_SetItemEdges(item, itemstart, newstart + fadelen)
-      end
-      r.SetMediaItemInfo_Value(item, "D_FADEOUTLEN_AUTO", fadelen)
+    local item_a, item_a_start, item_a_end
+    local item_b, item_b_start, item_b_end
+
+    if what == 'itemStart' then
+      item_a = item
+      item_a_start = itemstart
+      item_a_end = itemend
+      item_b = item2
+      item_b_start = item2start
+      item_b_end = item2end
+    elseif what == 'itemEnd' then
+      item_a = item2
+      item_a_start = item2start
+      item_a_end = item2end
+      item_b = item
+      item_b_start = itemstart
+      item_b_end = itemend
     end
+
+    r.Undo_BeginBlock2(0)
+
+    local newstart = item_a_start
+    if item_a_start > item_b_end - justlen then
+      newstart = item_b_end - justlen
+      if newstart < item_b_start then
+        newstart = item_b_start + 0.0001 -- some tiny amount
+      end
+      r.BR_SetItemEdges(item_a, newstart, item_a_end)
+    elseif item_a_start < item_b_end then
+      fadelen = item_b_end - item_a_start
+    end
+    r.SetMediaItemInfo_Value(item_a, 'D_FADEINLEN_AUTO', fadelen)
+    local newend = newstart + fadelen
+    if item_b_end < newend then
+      if item_a_end < newend then
+        newend = item_a_end - 0.0001 -- some tiny amount
+      end
+      r.BR_SetItemEdges(item_b, item_b_start, newend)
+    end
+    r.SetMediaItemInfo_Value(item_b, 'D_FADEOUTLEN_AUTO', fadelen)
+
     r.UpdateArrange()
   end
 

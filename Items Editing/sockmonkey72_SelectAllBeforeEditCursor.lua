356525e6ddbb57e1a8839ca6a545577640d308e1
Jeremy Bernstein <jeremy.d.bernstein@googlemail.com>
2024-01-12 10:07:09 +0100

    Add before/after selection scripts

---
+++ Items Editing/sockmonkey72_SelectAllBeforeEditCursor.lua
@@ -0,0 +1,45 @@
+-- @description sockmonkey72_Select items before/after edit cursor
+-- @author sockmonkey72
+-- @version 1.0
+-- @about
+--   # A small set of scripts to select items before/after the edit cursor
+-- @provides
+--   [main] sockmonkey72_SelectAllBeforeEditCursor.lua
+--   [main] sockmonkey72_SelectAllAfterEditCursor.lua
+-- @changelog
+--   initial
+
+local r = reaper
+
+r.Undo_BeginBlock2(0)
+r.PreventUIRefresh(1)
+
+local ct = r.CountSelectedMediaItems(0)
+for i = ct - 1, 0, -1 do
+    local item = r.GetSelectedMediaItem(0, i)
+    if item then
+        r.SetMediaItemSelected(item, false)
+    end
+end
+
+local position = r.GetCursorPositionEx(0)
+
+ct = r.CountMediaItems(0)
+for i = 0, ct -1 do
+    local item = r.GetMediaItem(0, i)
+    if item then
+        local starttime = r.GetMediaItemInfo_Value(item, 'D_POSITION')
+        if starttime < position then
+            r.SetMediaItemSelected(item, true)
+        -- else
+        --      local duration = r.GetMediaItemInfo_Value(item, 'D_LENGTH')
+        --      if starttime + duration >= position then
+        --         r.SetMediaItemSelected(item, true)
+        --      end
+        end
+    end
+end
+
+r.PreventUIRefresh(-1)
+r.UpdateArrange()
+r.Undo_EndBlock2(0, "Select all items after edit cursor", -1)

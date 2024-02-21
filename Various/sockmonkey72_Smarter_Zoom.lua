2b0750d3932c084ff3949754f3ed7976b15d2e5d
Jeremy Bernstein <jeremy.d.bernstein@googlemail.com>
2024-02-21 12:37:03 +0100

    Smarter Zoom -> v1.4

--- Various/sockmonkey72_Smarter_Zoom.lua
+++ Various/sockmonkey72_Smarter_Zoom.lua
@@ -1,31 +1,37 @@
 -- @description Smarter Zoom
 -- @author sockmonkey72
--- @version 1.03
--- @changelog 1.03 initial upload
+-- @version 1.04
+-- @changelog
+--  * when zooming tracks, use the SWS zoom + minimize others action
+--  * fix undo strings
+--  * increase margin buffer a little bit
+--  * fix some Lua warnings & improve comments
 -- @about Zoom and scroll to razor edit region, item selection or time selection. Requires JS and SWS extensions
 
-local reaper = reaper
+local r = reaper
 
 local doRE = false
 local doItems = false
 local doTimeSel = false
 
-local trackview = reaper.JS_Window_FindChildByID(reaper.GetMainHwnd(), 1000)
-local _, arrange_width, arrange_height = reaper.JS_Window_GetClientSize(trackview)
+local trackview = r.JS_Window_FindChildByID(r.GetMainHwnd(), 1000)
+local _, arrange_width, arrange_height = r.JS_Window_GetClientSize(trackview)
 
-local trackCount = reaper.CountTracks(0)
+local trackCount = r.CountTracks(0)
 local firstTrack = -1
 local lastTrack = -1
 
 local REtracks = {}
 
- minTime = 0xFFFFFFFFF -- whatever, something big
- maxTime = 0
+local minTime = tonumber(0xFFFFFFFFF) -- whatever, something big
+local maxTime = tonumber(0)
+
+local bufferscale = 0.15 -- might need to increase this on other platforms
 
 for i = 0, trackCount-1 do
-  local track = reaper.GetTrack(0, i)
-  if track and reaper.IsTrackVisible(track, false) then
-    local rv, razorEdits = reaper.GetSetMediaTrackInfo_String(track, "P_RAZOREDITS", "", false)
+  local track = r.GetTrack(0, i)
+  if track and r.IsTrackVisible(track, false) then
+    local rv, razorEdits = r.GetSetMediaTrackInfo_String(track, "P_RAZOREDITS", "", false)
     if rv == true and razorEdits ~= "" then
       if firstTrack == -1 then firstTrack = i
       else lastTrack = i end
@@ -54,57 +60,55 @@ end
 if #REtracks ~= 0 then doRE = true end
 
 if doRE == false then
-  numSelectedItems = reaper.CountSelectedMediaItems(0)
+  local numSelectedItems = r.CountSelectedMediaItems(0)
   if numSelectedItems > 0 then doItems = true
   else
-    minTime, maxTime = reaper.GetSet_LoopTimeRange2(0, false, false, 0, 0, false)
+    minTime, maxTime = r.GetSet_LoopTimeRange2(0, false, false, 0, 0, false)
     doTimeSel = minTime ~= maxTime and true or false
   end
 end
 
 if doRE == true or doItems == true  or doTimeSel == true then
-  reaper.PreventUIRefresh(1)
-
-  reaper.Undo_BeginBlock2(0)
+  r.Undo_BeginBlock2(0)
 
   if doRE == true then
+    r.PreventUIRefresh(1)
     local selectedTracks = {}
-    local selCount = reaper.CountSelectedTracks(0)
+    local selCount = r.CountSelectedTracks(0)
     for i = 0, selCount - 1 do
-      selectedTracks[#selectedTracks + 1] = reaper.GetSelectedTrack(0, i)
+      selectedTracks[#selectedTracks + 1] = r.GetSelectedTrack(0, i)
     end
 
-    reaper.Main_OnCommandEx(40297, 0, 0) -- unselect all tracks
+    r.Main_OnCommandEx(40297, 0, 0) -- Track: Unselect (clear selection of) all tracks
 
     for _, track in pairs(REtracks) do
-      if track then reaper.SetTrackSelected(track, true) end
+      if track then r.SetTrackSelected(track, true) end
     end
 
-    reaper.PreventUIRefresh(-1)
+    r.PreventUIRefresh(-1)
 
-    reaper.Main_OnCommandEx(40913, 0, 0)
-    reaper.Main_OnCommandEx(reaper.NamedCommandLookup("_SWS_VZOOMFIT"), 0, 0) -- vert zoom to selected tracks
+    r.Main_OnCommandEx(40913, 0, 0) -- Track: Vertical scroll selected tracks into view
+    r.Main_OnCommandEx(r.NamedCommandLookup("_SWS_VZOOMFITMIN"), 0, 0) -- SWS: Vertical zoom to selected tracks, minimize others
 
-    local buffer = (maxTime - minTime) * 0.01
-    reaper.GetSet_ArrangeView2(0, true, 0, 0, minTime - buffer, maxTime + 2 * buffer)
+    local buffer = (maxTime - minTime) * bufferscale -- might need to increase this on other platforms
+    r.GetSet_ArrangeView2(0, true, 0, 0, minTime - buffer, maxTime + buffer)
 
-    reaper.PreventUIRefresh(1)
+    r.PreventUIRefresh(1)
 
-    reaper.Main_OnCommandEx(40297, 0, 0) -- unselect all tracks
+    r.Main_OnCommandEx(40297, 0, 0) -- Track: Unselect (clear selection of) all tracks
 
     for _, track in pairs(selectedTracks) do
-      reaper.SetTrackSelected(track, true)
+      r.SetTrackSelected(track, true)
     end
+    r.PreventUIRefresh(-1)
   elseif doItems == true then
-    reaper.Main_OnCommandEx(41622, 0, 0)
+    r.Main_OnCommandEx(r.NamedCommandLookup("_SWS_ITEMZOOMMIN"), 0, 0) -- SWS: Vertical zoom to selected items, minimize others
   elseif doTimeSel == true then
-    local buffer = (maxTime - minTime) * 0.01
-    reaper.GetSet_ArrangeView2(0, true, 0, 0, minTime - buffer, maxTime + 2 * buffer)
+    local buffer = (maxTime - minTime) * bufferscale
+    r.GetSet_ArrangeView2(0, true, 0, 0, minTime - buffer, maxTime + buffer)
   end
 
-  reaper.PreventUIRefresh(-1)
+  r.Undo_EndBlock2(0, "Smarter Zoom: " .. (doRE and "Razor Edit" or doItems and "Items" or doTimeSel and "Time Selection" or "(no-op)"), -1)
 
-  reaper.Undo_EndBlock2(0, "Zoom to Razor Edit", -1)
-
-  reaper.UpdateTimeline()
+  r.UpdateTimeline()
 end

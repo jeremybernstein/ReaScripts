49540a91cdbbaffae19f5535a2a4de508236569c
Jeremy Bernstein <jeremy.d.bernstein@googlemail.com>
2024-03-14 10:46:32 +0100

    Event Editor -> 1.3 (MIDIUtils update)

--- MIDI Editor/sockmonkey72_EventEditor.lua
+++ MIDI Editor/sockmonkey72_EventEditor.lua
@@ -1,5 +1,5 @@
 -- @description MIDI Event Editor
--- @version 1.2
+-- @version 1.3
 -- @author sockmonkey72
 -- @about
 --   # MIDI Event Editor
@@ -13,7 +13,7 @@
 --   When setting negative absolute values for pitch bend, use --VALUE (instead of -VALUE, which will use the
 --   '-' Math Operator)
 -- @changelog
---   - add support for text/sysex/notation events
+--   - MIDIUtils update
 -- @provides
 --   EventEditor/MIDIUtils.lua https://raw.githubusercontent.com/jeremybernstein/ReaScripts/main/MIDI/MIDIUtils.lua
 --   [main=midi_editor,midi_eventlisteditor,midi_inlineeditor] sockmonkey72_EventEditor.lua

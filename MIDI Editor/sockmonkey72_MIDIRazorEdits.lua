b015eb3eb88cc9b8a08567f0d683b2aa1d0858e3
Jeremy Bernstein <jeremy.d.bernstein@googlemail.com>
2026-01-26 13:37:40 +0100

    MIDI Razor Edits -> v1.5.0-beta.7

--- MIDI Editor/sockmonkey72_MIDIRazorEdits.lua
+++ MIDI Editor/sockmonkey72_MIDIRazorEdits.lua
@@ -1,11 +1,14 @@
 -- @description MIDI Razor Edits
--- @version 1.5.0-beta.6
+-- @version 1.5.0-beta.7
 -- @author sockmonkey72
 -- @about
 --   # MIDI Razor Edits
 -- @changelog
---   - fix childwindow detection and warnings on non-Windows
---   - add guards to childwindow-only function calls so the script doesn't crash when it's not present
+--   - add color pickers for pitchbend curve/point colors
+--   - fix behavior when selection goes out of bounds in pitchbend mode
+--   - some fixes for switching modes between main, slicer, pitchbend (and preventing same when dedicated scripts in use)
+--   - minor cursor/focus fixups
+--   - now using custom menu function where appropriate to work around gfx.showmenu multiple monitor limitations
 -- @provides
 --   {RazorEdits}/*
 --   {RazorEdits}/{lib}/{lua-scala}/*
